3a39014896c4445486f1e0457a93ff970886a9a2
Alex Kinnear <alexander.kinnear@aggiemail.usu.edu>
2021-05-04 20:38:01 -0600

    Classic Snake game finished

---
+++ main.py
@@ -0,0 +1,66 @@
+import pygame
+from copy import deepcopy
+from snake import Snake, Food
+
+WIDTH, HEIGHT = 600, 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+CLOCK = pygame.time.Clock()
+
+snake = Snake()
+food = Food()
+
+
+def __init_window():
+    pygame.display.set_caption('Snake Game')
+    background_colour = (255, 255, 255)
+    SCREEN.fill(background_colour)
+
+
+def draw_snake():
+    for block in snake.blocks:
+        pygame.draw.rect(SCREEN, snake.color, [block.x, block.y, snake.width, snake.height])
+
+
+if __name__ == "__main__":
+    __init_window()
+
+    running = True
+    while running:
+        temp = deepcopy(snake.blocks)
+        for i in range(1, len(snake.blocks)):
+            snake.blocks[i].x = temp[i-1].x
+            snake.blocks[i].y = temp[i-1].y
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                keyPressed = True
+                if event.key == pygame.K_LEFT:
+                    snake.head.dx = -snake.width
+                    snake.head.dy = 0
+                elif event.key == pygame.K_RIGHT:
+                    snake.head.dx = snake.width
+                    snake.head.dy = 0
+                elif event.key == pygame.K_UP:
+                    snake.head.dx = 0
+                    snake.head.dy = -snake.height
+                elif event.key == pygame.K_DOWN:
+                    snake.head.dx = 0
+                    snake.head.dy = snake.height
+
+        snake.head.x += snake.head.dx
+        snake.head.y += snake.head.dy
+
+        SCREEN.fill((255, 255, 255))
+        pygame.draw.rect(SCREEN, food.color, [food.x, food.y, snake.width, snake.height])
+        draw_snake()
+        pygame.display.flip()
+        CLOCK.tick(snake.vel)
+
+        running = not snake.is_dead(WIDTH, HEIGHT)
+
+        # Check if eating food
+        if snake.head.x == food.x and snake.head.y == food.y:
+            food.get_new_pos()
+            snake.add_block()
